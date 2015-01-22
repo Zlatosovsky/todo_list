3a955192b104692efaff7da4d75242b46be2e925
Zlatosovsky <zlatosovsky@gmail.com>
2015-01-22 18:36:32 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Used for verifying the integrity of signed cookies.
+# After changing this key, all old signed cookies will become invalid!
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+TodoList::Application.config.secret_key_base = secure_token
